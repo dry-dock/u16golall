a91d66304f25259b424e67eb450804e434474f0f
Niranjan Godbole <niranjan8192@gmail.com>
2018-01-17 12:09:07 +0000

    update golang versions

--- go_install.sh
+++ go_install.sh
@@ -9,7 +9,7 @@ gvm install go1.4 && gvm use go1.4 && go install -a std && go get -u github.com/
 
 
 
-declare -a versions=( '1.7.6' '1.8.3' '1.9.1' )
+declare -a versions=( '1.7.6' '1.8.5' '1.9.2' )
 for version in "${versions[@]}"
   do
      echo "================= Install Go $version==================="
