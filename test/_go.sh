633b447881e6f3b2bf630be96a5f58293e5eb066
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-04-02 17:33:37 +0530

    update the latest versions

--- test/_go.sh
+++ test/_go.sh
@@ -3,7 +3,7 @@
 #set the source path of gvm. $HOME points to '/root'
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm
 
-declare -a versions=( '1.7.6' '1.8.7' '1.9.4' '1.10' )
+declare -a versions=( '1.7.6' '1.8.7' '1.9.5' '1.10.1' )
 for version in "${versions[@]}"
   do
     echo "######### Testing Go$version ###################"
