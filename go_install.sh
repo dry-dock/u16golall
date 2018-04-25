a5045fe8f60ceaf8c430a9e44410781c94b4052b
Devashish <devashish.86@gmail.com>
2018-04-25 13:53:09 +0530

    fixing golang

--- go_install.sh
+++ go_install.sh
@@ -2,18 +2,20 @@
 
 #set the source path of gvm. $HOME points to '/root'
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm
-export  CGO_ENABLED=0
+export CGO_ENABLED=0
 
-
-gvm install go1.4 && gvm use go1.4 && go install -a std && go get -u github.com/tools/godep
-
-
+gvm install go1.4
+gvm use go1.4
+go install -a std
+go get -u github.com/tools/godep
 
 declare -a versions=( '1.7.6' '1.8.7' '1.9.5' '1.10.1' )
 for version in "${versions[@]}"
   do
      echo "================= Install Go $version==================="
-     gvm use go1.4.2
      export GOROOT_BOOTSTRAP=$GOROOT
-     gvm install go$version && gvm use go$version && go install -a std && go get -u github.com/tools/godep
+     gvm install go$version
+     gvm use go$version
+     go install -a std
+     go get -u github.com/tools/godep
 done
