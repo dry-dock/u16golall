632d6d2bbb43bcd146b267e6503c8d002a807663
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-22 16:08:42 +0530

    Update go_install.sh

--- go_install.sh
+++ go_install.sh
@@ -13,7 +13,7 @@ gvm use go1.4
 go install -a std
 go get -u github.com/tools/godep
 
-declare -a versions=( '1.7.6' '1.8.7' '1.9.5' '1.10.1' )
+declare -a versions=( '1.7.6' '1.8.7' '1.9.6' '1.10.2' )
 for version in "${versions[@]}"
   do
     echo "================= Install Go $version==================="
