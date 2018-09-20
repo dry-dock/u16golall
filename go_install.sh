ebf4ea25ea7772ed9a1495fba65244c1868c112d
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-09-20 16:08:29 +0530

    Update go_install.sh

--- go_install.sh
+++ go_install.sh
@@ -4,22 +4,12 @@
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm
 export CGO_ENABLED=0
 
-export GVM_INSTALL=1.4
-echo "============= Installing Go $GVM_INSTALL ===================="
-gvm install go$GVM_INSTALL
-
-echo "=========== Using go  $GVM_INSTALL ==================="
-source /root/.gvm/scripts/gvm
-gvm use go$GVM_INSTALL
-go install -a std
-go get -u github.com/tools/godep
-
-declare -a versions=( '1.9.7' '1.11' )
+declare -a versions=( '1.11' )
 for version in "${versions[@]}"
   do
     echo "================= Install Go $version==================="
     export GOROOT_BOOTSTRAP=$GOROOT
-    gvm install go$version
+    gvm install go$version --prefer-binary
     source /root/.gvm/scripts/gvm
     gvm use go$version
     go install -a std
